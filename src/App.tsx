b9d5433bee2d042a5094fa886df747d0f45c6cd2
Dave86dev <davidochando1986@gmail.com>
2024-02-05 23:14:36 +0100

    feat: some design before next stage, proper json display center page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import './App.css'
-import { JsonRender } from './common/JsonRender/JsonRender'
-import jsonData from './data/demoData.json'
+import "./App.css";
+import { JsonRender } from "./common/JsonRender/JsonRender";
+import jsonData from "./data/demoData.json";
 
-import { useSelector } from 'react-redux';
-import { RootState } from './app/store';
-import { useEffect } from 'react';
+import { useSelector } from "react-redux";
+import { RootState } from "./app/store";
+import { useEffect } from "react";
 
 function App() {
-
   const { key, value } = useSelector((state: RootState) => state.keyValue);
 
-  useEffect(()=> {
-    console.log(key, value)
-  },[value])
-
+  useEffect(() => {
+    console.log(key, value);
+  }, [value]);
 
   return (
     <div className="mainContainer">
-      <JsonRender data={jsonData as unknown} depth={0}/>
+      <div className="jsonContainer">
+        <JsonRender data={jsonData as unknown} depth={0} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
